c9c924f82bf6bba273412db84e8d9d879a44bbeb
Katrina Behnken <katrina.behnken@gmail.com>
2020-09-28 11:43:50 -0700

    Add RecipeCard

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,13 @@ import { PrivateRoute } from './Components/PrivateRoutes.js';
 import Dashboard from './Components/Dashboard';
 import Login from './Components/Login';
 import Logout from './Components/Logout';
-// import RecipeCard from './Components/RecipeCard';
+import RecipeCard from './Components/RecipeCard';
 // import RecipeForm from './Components/RecipeForm';
 
 export default (
   <Switch>
     <PrivateRoute exact path='/' component={Dashboard} />
+    <PrivateRoute path='/recipe-card/:recipe_id' component={RecipeCard} />
     <Route path='/login' component={Login} />
     <Route path='/logout' component={Logout} />
   </Switch>
