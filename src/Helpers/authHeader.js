2582bb100413f4d315ac2a8bc93536b4432ce89e
Katrina Behnken <katrina.behnken@gmail.com>
2020-10-19 10:54:26 -0700

    Remove console.log

--- src/Helpers/authHeader.js
+++ src/Helpers/authHeader.js
@@ -1,6 +1,5 @@
 export function authHeader() {
     let accessToken = JSON.parse(localStorage.getItem('accessToken'));
-    // console.log(accessToken);
     if (accessToken) {
         return {
             Authorization: 'Bearer ' + accessToken
