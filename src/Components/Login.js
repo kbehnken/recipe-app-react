565d7ac325c9d6c21d8a08cef873e7e3fa5a470a
Katrina Behnken <katrina.behnken@gmail.com>
2020-11-21 10:51:47 -0700

    Add functionality to show/hide password

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,11 +2,19 @@ import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { login } from '../Redux/Actions/authActions';
 import { Redirect } from 'react-router-dom';
+import { BsEyeFill, BsEyeSlashFill } from 'react-icons/bs';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import '../Styles/login.css';
 import '../Styles/main.css';
 
 function Login(props) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const togglePasswordVisibility = () => {
+        return setShowPassword(!showPassword);
+    };
     if (props.loggedIn === true) {
         return (
             <Redirect to='/' />
@@ -17,13 +25,32 @@ function Login(props) {
         <div id='outer-login-container'>
             <div id='login-container'>
                 <div>
-                    <input type='text' placeholder='Enter email address' value={email} onChange={e => setEmail(e.target.value)}></input>
+                    <TextField required type='email' variant='outlined' label='Email Address' value={email} onChange={e => setEmail(e.target.value)} style={{width: '100%'}} />
                 </div>
+                {showPassword === false ?
+                    (
+                        <div>
+                            <TextField required type='password' name='password' variant='outlined' label='Password' value={password} onChange={e => setPassword(e.target.value)} style={{width: '100%'}} autoComplete='none' InputProps={{
+                                endAdornment:
+                                <InputAdornment position='end'>
+                                    <BsEyeSlashFill size={20} onClick={togglePasswordVisibility} style={{cursor: 'pointer', color: '#00b300'}} />
+                                </InputAdornment>
+                            }} />
+                        </div>
+                    ) :
+                    (
+                        <div>
+                            <TextField required name='password' variant='outlined' label='Password' value={password} onChange={e => setPassword(e.target.value)} style={{width: '100%'}} autoComplete='none' InputProps={{
+                                endAdornment:
+                                <InputAdornment position='end'>
+                                    <BsEyeFill size={20} onClick={togglePasswordVisibility} style={{cursor: 'pointer', color: '#00b300'}} />
+                                </InputAdornment>
+                            }} />
+                        </div>
+                    )
+                }
                 <div>
-                    <input type='password' placeholder='Enter password' value={password} onChange={e => setPassword(e.target.value)}></input>
-                </div>
-                <div>
-                    <button onClick={() => props.dispatch(login(email, password))}>
+                    <button className='login-button' onClick={() => props.dispatch(login(email, password))}>
                         Login
                     </button>
                 </div>
