d4e432009befc8d52253dc0ea0738c73e2b25b3a
Katrina Behnken <katrina.behnken@gmail.com>
2020-11-22 15:24:29 -0700

    Add user and hamburger menus

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,23 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaRegUser } from 'react-icons/fa';
+import { GiHamburgerMenu } from 'react-icons/gi'
 import '../Styles/nav.css'
 
-export default function Nav(props) {
+export default function Nav() {
+    const [showMenu, setShowMenu] = useState(false);
+    const toggleMenuVisibility = () => {
+        return setShowMenu(!showMenu);
+    };
+
     return (
-        <nav>
-            <Link to='/all-recipes'>
-                Browse All Recipes
-            </Link>
-            <Link to='/add-recipe'>
-                Add a New Recipe
-            </Link>
-            <Link to='/edit-my-recipes'>
-                Edit My Recipes
-            </Link>
-            <Link to='/logout'>
-                <FaRegUser style={{ fill: '#00b300' }} />
-            </Link>
-        </nav>
+        <div>
+            <nav>
+                <div>
+                    <Link to='/'>
+                        Dashboard
+                    </Link>
+                </div>
+                <div>
+                    <Link to='/all-recipes'>
+                        Browse All Recipes
+                    </Link>
+                </div>
+                <div>
+                    <Link to='/add-recipe'>
+                        Add a New Recipe
+                    </Link>
+                </div>
+                <div>
+                    <Link to='/my-recipes'>
+                        My Recipes
+                    </Link>
+                </div>
+                <div className='user-icon'>
+                    <FaRegUser onClick={toggleMenuVisibility} style={{cursor: 'pointer'}} />
+                    {showMenu === true ?
+                        (
+                            <menu>
+                                <div>
+                                    <Link to='/profile'>
+                                        Manage User Profile
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link to='/logout'>
+                                        Logout
+                                    </Link>
+                                </div>
+                            </menu>
+                        ) :
+                        (
+                            null
+                        )
+                    }
+                </div>
+            </nav>
+            <div id='hamburger-nav'>
+                <div className='hamburger-icon' style={{width: '18px'}}>
+                    <GiHamburgerMenu onClick={toggleMenuVisibility} style={{cursor: 'pointer'}} />
+                    {showMenu === true ?
+                        (
+                            <menu>
+                                <div>
+                                    <Link to='/'>
+                                        Dashboard
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link to='/all-recipes'>
+                                        Browse All Recipes
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link to='/add-recipe'>
+                                        Add a New Recipe
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link to='/my-recipes'>
+                                        My Recipes
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link to='/profile'>
+                                        Manage User Profile
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link to='/logout'>
+                                        Logout
+                                    </Link>
+                                </div>
+                            </menu>
+                        ) :
+                        (
+                            null
+                        )
+                    }
+                </div>
+            </div>
+        </div>
     );    
 }
