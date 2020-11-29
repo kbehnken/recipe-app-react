f48565e0d8bf93d76c7589fb8865959e012f9adb
Katrina Behnken <katrina.behnken@gmail.com>
2020-11-28 17:10:15 -0700

    Add update-recipes and profile

--- src/Routes.js
+++ src/Routes.js
@@ -3,21 +3,25 @@ import { Switch, Route } from 'react-router-dom';
 import { PrivateRoute } from './Components/PrivateRoutes.js';
 import Dashboard from './Components/Dashboard';
 import AllRecipes from './Components/AllRecipes'
-import RecipeForm from './Components/RecipeForm';
+import CreateRecipeForm from './Components/CreateRecipeForm';
+import UpdateRecipeForm from './Components/UpdateRecipeForm'
 import MyRecipes from './Components/MyRecipes'
 import RecipeCard from './Components/RecipeCard';
+import SearchResults from './Components/SearchResults.js';
+import Profile from './Components/Profile';
 import Login from './Components/Login';
 import Logout from './Components/Logout';
-import SearchResults from './Components/SearchResults.js';
 
 export default (
     <Switch>
         <PrivateRoute exact path='/' component={Dashboard} />
         <PrivateRoute path='/all-recipes' component={AllRecipes} />
-        <PrivateRoute path='/add-recipe' component={RecipeForm} />
+        <PrivateRoute path='/add-recipe' component={CreateRecipeForm} />
         <PrivateRoute path='/my-recipes' component={MyRecipes} />
+        <PrivateRoute path='/update-recipes/:recipe_id' component={UpdateRecipeForm} />
         <PrivateRoute path='/recipe-card/:recipe_id' component={RecipeCard} />
         <PrivateRoute path='/search-results' component={SearchResults} />
+        <PrivateRoute path='/profile' component={Profile} />
         <Route path='/login' component={Login} />
         <Route path='/logout' component={Logout} />
     </Switch>
