5400f7d55ebb43af01ff257b6b3913fc80a60f40
Katrina Behnken <katrina.behnken@gmail.com>
2020-11-28 17:51:31 -0700

    Add Search and RecipeTile components

--- src/Components/SearchResults.js
+++ src/Components/SearchResults.js
@@ -1,44 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Search from './Search'
+import RecipeTile from './RecipeTile';
 import Nav from './Nav';
 import '../Styles/main.css';
 
 function SearchResults(props) {
     const mappedRecipes = props.recipes.map((item) => {
         return (
-            <div key={item.recipe_id}>
-                <div>
-                    <h2>
-                        <Link to={{pathname: '/recipe-card/' + item.recipe_id}}>
-                            {item.recipe_name}
-                        </Link>
-                    </h2>
-                    {item.photo_url}<br />
-                    <label>Contributor: </label>{item.contributor}<br />
-                    <label>Prep Time: </label>{item.prep_time}<br />
-                    <label>Cook Time: </label>{item.cook_time}<br />
-                </div>
-            </div>
+            <RecipeTile  key={item.recipe_id} recipe={item} />
         );
     })
     
     return(
-        <div className='outer-content-container'>
+        <div>
             <div>
+                <Nav />
+            </div>
+            <div className='outer-content-container'>
                 <div>
-                    <Nav />
+                    <Search />
                 </div>
-                {mappedRecipes.length === 0 ?
-                    (
-                        <p>There are no recipes that match your search criteria. <Link to='all-recipes'>Click here</Link> to browse all recipes.</p>
-                    ) :
-                    (
-                        <div>
-                            {mappedRecipes}
-                        </div>
-                    )
-                }
+                <div>
+                    {mappedRecipes.length === 0 ?
+                        (
+                            <p>There are no recipes that match your search criteria. <Link to='all-recipes'>Click here</Link> to browse all recipes.</p>
+                        ) :
+                        (
+                            <div>
+                                {mappedRecipes}
+                            </div>
+                        )
+                    }
+                </div>
             </div>
         </div>
     );
