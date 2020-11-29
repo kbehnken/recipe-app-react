6bc7684e488cbdefeea132f2b587e15df3057dca
Katrina Behnken <katrina.behnken@gmail.com>
2020-11-28 17:21:34 -0700

    Change html inputs to material-ui inputs

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,22 +1,32 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom'
 import { connect, useDispatch } from 'react-redux';
+import { BsSearch } from 'react-icons/bs';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import { getSearchResults } from '../Redux/Actions/recipeActions';
 
-function Search(props) {
+function Search() {
     const [search, setSearch] = useState('');
     const dispatch = useDispatch();
     const history = useHistory();
 
     return (
-        <div>
-            <input type='text' name='search' placeholder='Search all recipes' value={search} onChange={e => setSearch(e.target.value)} />
-            <button onClick={() => {
-                dispatch(getSearchResults(search))
-                .then(history.push('/search-results'));
-            }}>
-                Search
-            </button>
+        <div className='form-container'>
+            <TextField variant='outlined' label='Search all recipes' onKeyPress={e => {
+                if (e.key === 'Enter') {
+                    dispatch(getSearchResults(search))
+                    .then(history.push('/search-results'));
+                }
+            }} onChange={e => setSearch(e.target.value)} InputProps={{
+                endAdornment:
+                <InputAdornment position='end'>
+                    <BsSearch title='search' size={20} style={{cursor: 'pointer', color: '#00b300'}} onClick={() => {
+                        dispatch(getSearchResults(search))
+                        .then(history.push('/search-results'));
+                    }} />
+                </InputAdornment>
+            }} />
         </div>
     )
 }
