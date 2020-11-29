b6c72393f69ca0a1eb96ecc8c25d537ec7bd3333
Katrina Behnken <katrina.behnken@gmail.com>
2020-11-28 17:16:25 -0700

    Add NotificationManager and fix Redirect

--- src/Components/PrivateRoutes.js
+++ src/Components/PrivateRoutes.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import NotificationManager from 'react-notifications/lib/NotificationManager';
 import { Route, Redirect } from 'react-router-dom';
 import { isLoggedIn } from '../Helpers/isLoggedIn';
 import { refreshLogin } from '../Redux/Actions/authActions';
 
-export const PrivateRoute = ({component: Component, ...rest }) => (
-    <Route {...rest} render = {props => (
-        isLoggedIn() || refreshLogin()
-            ? <Component {...props} />
-            : <Redirect to = {{ pathname: '/login', state: { from: props.location }}} />
+export const PrivateRoute = ({component: Component, ...rest }) => {
+    if (!isLoggedIn()) {
+        NotificationManager.error('Your login has expired. Please log in again.')
+    }
+    return <Route {...rest} render = {props => (
+        isLoggedIn() //|| refreshLogin()
+            ? (<Component {...props} />)
+            : (<Redirect to = '/logout' />)
     )} />
-)
+}
