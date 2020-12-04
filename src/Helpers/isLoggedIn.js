78bddf8d61773c610dfaa66b39f7f3e1b038ab4b
Katrina Behnken <katrina.behnken@gmail.com>
2020-12-03 18:28:33 -0700

    #3 Remove state.auth.loggedIn check

--- src/Helpers/isLoggedIn.js
+++ src/Helpers/isLoggedIn.js
@@ -1,10 +1,12 @@
 import jwt_decode from 'jwt-decode';
+import { loginSuccess } from '../Redux/Actions/authActions';
 import store from './../Redux/store';
 
 export function isLoggedIn() {
     let accessToken = '';
     let tokenExp = '';
-    let state = store.getState();
+    // let state = store.getState();
+    let dispatch = store.dispatch;
 
     if ('accessToken' in localStorage) {
         accessToken = localStorage.getItem('accessToken');
@@ -12,11 +14,13 @@ export function isLoggedIn() {
     } else {
         return false;
     }
-    if (state.auth.loggedIn === false) {
-        return false;
-    }
+    // if (state.auth.loggedIn === false) {
+    //     return false;
+    // }
     if (tokenExp * 1000 <= Date.now()) {
         return false;
+    } else {
+        dispatch(loginSuccess())
     }
     return true;
 }
