79eedfa6426539fee6e401c09dc6fdae56269a0f
Katrina Behnken <katrina.behnken@gmail.com>
2021-09-22 12:40:43 -0700

    Add welcome message

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { getUserInfo } from '../Helpers/getUserInfo';
 import Nav from './Nav';
 
 function Header() {
+    
     return (
         <header>
+            <div>
+                Welcome, {getUserInfo().first_name}!
+            </div>
             <Nav />
         </header>
     );
