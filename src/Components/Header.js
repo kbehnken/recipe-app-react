fa216d4d9d836894342299626dd67a4b86d25f3f
Katrina Behnken <katrina.behnken@gmail.com>
2021-09-29 10:06:44 -0700

    Add <Search> and css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { getUserInfo } from '../Helpers/getUserInfo';
+import Search from './Search';
 import Nav from './Nav';
 
 function Header() {
     
     return (
         <header>
-            <div>
-                Welcome, {getUserInfo().first_name}!
+            <div className='flex-between'>
+                <div>
+                    Welcome, {getUserInfo().first_name}!
+                </div>
+                <Search />
             </div>
             <Nav />
         </header>
