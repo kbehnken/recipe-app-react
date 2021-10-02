ecf5a7c99d7ba287107a87431c58698c57559760
Katrina Behnken <katrina.behnken@gmail.com>
2021-10-02 07:46:21 -0700

    Initial commit

---
+++ src/Components/RecipeList.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import RecipeTile from './RecipeTile';
+import PaddingTile from './PaddingTile';
+
+function RecipeList(props) {
+    const cols = 3;
+    const mappedRecipes = props.recipes.map((item) => {
+        return (
+            <RecipeTile key={item.recipe_id} recipe={item} />
+        );
+    })
+
+    const padding = (() => {
+        if (mappedRecipes.length % cols === 0) {
+            return 0
+        }
+        return cols - (mappedRecipes.length % cols);
+    })()
+    for(let i = 0; i <= padding; i++) {
+        mappedRecipes.push(<PaddingTile />)
+    }
+
+    return(
+        <div>
+            {mappedRecipes.length === 0 ?
+                (
+                    <p style={{textAlign: 'center'}}>
+                        You have no recipes to display. <Link to='all-recipes'>Click here</Link> to browse all recipes.
+                    </p>
+                ) :
+                (
+                    <div>
+                        <div className='flex-between-wrap'>
+                            {mappedRecipes}
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+export default RecipeList
